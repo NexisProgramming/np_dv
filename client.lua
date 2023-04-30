df49c0b7dc718767dd7ee58de484be97279d823e
Dusty05 <123284326+Dustyy05@users.noreply.github.com>
2023-04-30 14:59:28 +1000

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,50 +1,58 @@
+-- Car Command
+-- Define the Ace permission required to use the command
 RegisterCommand('car', function(source, args)
-    --account for the argument not being passed
+  -- Check if the player has the required Ace permission
+  if IsPlayerAceAllowed(source, ScriptReaper.Spawn_Car_Permission) then
+    -- Get the vehicle name from the arguments, or use a default if no argument was provided
     local vehicleName = args[1] or 'adder'
 
-    --check if vehicle actually exists
+    -- Check if the vehicle exists in the game
     if not IsModelInCdimage(vehicleName) or not IsModelAVehicle(vehicleName) then
-        TriggerEvent('chat:addMessage', {
-            args = {'Vehicle is not in this server' ..vehicleName}
-        })
-        return
+      TriggerClientEvent('chat:addMessage', source, { args = { '^1Error', 'This vehicle does not exist in the game.' } })
+      return
     end
 
-    --load the model
+    -- Load the vehicle model
     RequestModel(vehicleName)
-	
-        --wait for model to load
-        while not HasModelLoaded(vehicleName) do
-            Wait(500)
-        end
+    while not HasModelLoaded(vehicleName) do
+      Wait(500)
+    end
 
-    --get location
-    local playerPed = PlayerPedId() --get local player ped
-    local pos = GetEntityCoords(playerPed)-- get posistion of player
-
-    --create vehicle
+    -- Get the player's position and create the vehicle at that location
+    local playerPed = GetPlayerPed(source)
+    local pos = GetEntityCoords(playerPed)
     local vehicle = CreateVehicle(vehicleName, pos.x, pos.y, pos.z, GetEntityHeading(playerPed), true, false)
 
-    --put player into the driver seat
+    -- Put the player into the driver seat
     SetPedIntoVehicle(playerPed, vehicle, -1)
 
-    --give vehicle back to game
+    -- Release the vehicle and model resources
     SetEntityAsNoLongerNeeded(vehicle)
-
-    --release model
     SetModelAsNoLongerNeeded(vehicleName)
 
-    --tell player its in
-    TriggerEvent('chat:addMessage', {
-        args = {'You have spawned a ' .. vehicleName .. '.'}
-    })
+    -- Send a success message to the client
+    TriggerClientEvent('chat:addMessage', source, { args = { '^1Success', 'You have spawned a ' .. vehicleName .. '.' } })
+  else
+    -- Show an error message to the player if they don't have the permission
+    TriggerClientEvent('chat:addMessage', source, { args = { '^1Error', 'You do not have permission to use this command.' } })
+  end
 end, false)
 
-RegisterCommand('dv', function()
-    --get local player ped
-    local playerPed = PlayerPedId()
-    --get vehicle
+
+
+-- DV Command
+RegisterCommand('dv', function(source, args, rawCommand)
+    -- Get the player's server ID and Ped
+    local playerId = tonumber(source)
+    local playerPed = GetPlayerPed(playerId)
+    -- Get the vehicle the player is in
     local vehicle = GetVehiclePedIsIn(playerPed, false)
-    --delete vehicle
-    DeleteEntity(vehicle)
+    if DoesEntityExist(vehicle) then
+      -- Delete the vehicle and show a success message to the player
+      DeleteEntity(vehicle)
+      TriggerClientEvent('chat:addMessage', playerId, { args = { '^1Success', 'Your vehicle has been deleted.' } })
+    else
+      -- Show an error message to the player
+      TriggerClientEvent('chat:addMessage', playerId, { args = { '^1Error', 'You are not in a vehicle.' } })
+  end
 end, false)
