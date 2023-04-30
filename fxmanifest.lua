71fa8c31fc4a984c03c63322a2e397c7d8530fd4
Dusty05 <123284326+Dustyy05@users.noreply.github.com>
2023-04-30 14:59:02 +1000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,9 @@
 fx_version 'cerulean'
 game 'gta5'
-
 author 'Script Reaper'
-description 'My first Script'
-version '1.0.0'
+description 'Delete Vehicle Command'
+version '1.0'
 
-client_script 'client.lua'
+-- LUA Code
+server_script 'server.lua'
+shared_script 'config.lua'
